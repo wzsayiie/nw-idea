10acf490b54b2fac488d766a0127ed00cc47bef3
wujia <jia.wu@pitayagames.com>
2024-01-19 03:33:22 +0800

    removed type conversion.

--- cpp/dash/danycast.h
+++ cpp/dash/danycast.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <any>
-#include <cstdint>
 
 namespace dash {
 
@@ -14,25 +13,6 @@ template<class Type> struct cast {
         }
     }
 };
-
-template<class Type> struct n_cast {
-    Type operator=(const std::any &value) {
-        const std::type_info &type = value.type();
-
-        if (type == typeid(short  )) { return (Type)std::any_cast<short  >(value); }
-        if (type == typeid(int    )) { return (Type)std::any_cast<int    >(value); }
-        if (type == typeid(int64_t)) { return (Type)std::any_cast<int64_t>(value); }
-        if (type == typeid(float  )) { return (Type)std::any_cast<float  >(value); }
-        if (type == typeid(double )) { return (Type)std::any_cast<double >(value); }
-
-        return Type();
-    }
-};
-template<> struct cast<short  > : n_cast<short  > { using n_cast<short  >::operator=; };
-template<> struct cast<int    > : n_cast<int    > { using n_cast<int    >::operator=; };
-template<> struct cast<int64_t> : n_cast<int64_t> { using n_cast<int64_t>::operator=; };
-template<> struct cast<float  > : n_cast<float  > { using n_cast<float  >::operator=; };
-template<> struct cast<double > : n_cast<double > { using n_cast<double >::operator=; };
 
 } //end dash.
 
