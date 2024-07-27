adb4a76a740987262e037a1ec311793b88b4a286
wujia <wzsayiie@qq.com>
2024-07-27 23:17:05 +0800

    remove module hpc(host procedure calling).

--- cpp/hpc/HPC.cpp
+++
@@ -1,2 +0,0 @@
-void hpc_main() {
-}
--- cpp/hpc/hsession.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "hparam.h"
-
-class HSession : public dash::extends<HSession, HParam> {
-public:
-    void callback(const std::map<std::string, std::any> &args);
-    void callback(const std::vector<std::any> &args);
-};
