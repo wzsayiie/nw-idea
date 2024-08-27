ea71c5bb6c1c3e26cb52aa1e98eaa27ed444aaba
wujia <jia.wu@pitayagames.com>
2024-08-27 14:39:12 +0800

    added unix host.

---
+++ host/unix/main.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>
+#include "denviron.h"
+#include "lcall.h"
+
+#if D_OS_WINDOWS
+    #include <synchapi.h>
+#else
+    #include <unistd.h>
+#endif
+
+void sleep_sec(double sec) {
+    #if D_OS_WINDOWS
+        Sleep((DWORD)(sec * 1000));
+    #else
+        usleep((useconds_t)(sec * 1000 * 1000));
+    #endif
+}
+
+void on_exit();
+
+int main() {
+    //line mode.
+    #ifdef custom_main
+        custom_main();
+        return;
+    #endif
+
+    //loop mode.
+    low::call("_MAppStart");
+    low::call("_MAppResume");
+    atexit(on_exit);
+
+    low::call("_MAppUpdateInterval");
+    double sec = low::dr;
+
+    while (true) {
+        low::call("_MAppUpdate");
+        sleep_sec(sec);
+    }
+}
+
+void on_exit() {
+    low::call("_MAppPause");
+    low::call("_MAppStop");
+}
